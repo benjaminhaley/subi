f539ad56be93ca0b9f175ff84deab3eb96871133
Benjamin Haley <benjamin.haley@gmail.com>
2011-12-02 05:26:09 -0600

    Added instruction urls to extend py2app

--- run.bash
+++ run.bash
@@ -2,6 +2,9 @@
 #
 # Should build and run the stand alone app
 # requires py2app is installed.
+#
+# Instructions for py2app is at
+# http://bit.ly/m0F2mC
 
 rm setup.py
 py2applet --make-setup --argv-emulation=False subi.py
