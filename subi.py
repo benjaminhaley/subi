05b0fe6b013abdb1f046e28444f9538759cdb4f4
Benjamin Haley <benjamin.haley@gmail.com>
2011-12-02 08:51:00 -0600

    added code to start a webserver when the app is run, it is a simple example, needs to be modified

--- subi.py
+++ subi.py
@@ -1,92 +1,64 @@
-#!/usr/bin/env python
-# encoding: utf-8
-"""
-tkentrycomplete.py
+#Copyright Jon Berg , turtlemeat.com
 
-A tkinter widget that features autocompletion.
+import string,cgi,time
+from os import curdir, sep
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+#import pri
 
-Created by Mitja Martini on 2008-11-29.
-"""
-import sys
-import os
-import Tkinter
+class MyHandler(BaseHTTPRequestHandler):
 
-__version__ = "1.0"
+    def do_GET(self):
+        try:
+            if self.path.endswith(".html"):
+                f = open(curdir + sep + self.path) #self.path has /test.html
+#note that this potentially makes every file on your computer readable by the internet
 
-tkinter_umlauts=['odiaeresis', 'adiaeresis', 'udiaeresis', 'Odiaeresis', 'Adiaeresis', 'Udiaeresis', 'ssharp']
+                self.send_response(200)
+                self.send_header('Content-type',	'text/html')
+                self.end_headers()
+                self.wfile.write(f.read())
+                f.close()
+                return
+            if self.path.endswith(".esp"):   #our dynamic content
+                self.send_response(200)
+                self.send_header('Content-type',	'text/html')
+                self.end_headers()
+                self.wfile.write("hey, today is the" + str(time.localtime()[7]))
+                self.wfile.write(" day in the year " + str(time.localtime()[0]))
+                return
+                
+            return
+                
+        except IOError:
+            self.send_error(404,'File Not Found: %s' % self.path)
+     
 
-class AutocompleteEntry(Tkinter.Entry):
-        """
-        Subclass of Tkinter.Entry that features autocompletion.
-        
-        To enable autocompletion use set_completion_list(list) to define 
-        a list of possible strings to hit.
-        To cycle through hits use down and up arrow keys.
-        """
-        def set_completion_list(self, completion_list):
-                self._completion_list = completion_list
-                self._hits = []
-                self._hit_index = 0
-                self.position = 0
-                self.bind('<KeyRelease>', self.handle_keyrelease)               
+    def do_POST(self):
+        global rootnode
+        try:
+            ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
+            if ctype == 'multipart/form-data':
+                query=cgi.parse_multipart(self.rfile, pdict)
+            self.send_response(301)
+            
+            self.end_headers()
+            upfilecontent = query.get('upfile')
+            print "filecontent", upfilecontent[0]
+            self.wfile.write("<HTML>POST OK.<BR><BR>");
+            self.wfile.write(upfilecontent[0]);
+            
+        except :
+            pass
 
-        def autocomplete(self, delta=0):
-                """autocomplete the Entry, delta may be 0/1/-1 to cycle through possible hits"""
-                if delta: # need to delete selection otherwise we would fix the current position
-                        self.delete(self.position, Tkinter.END)
-                else: # set position to end so selection starts where textentry ended
-                        self.position = len(self.get())
-                # collect hits
-                _hits = []
-                for element in self._completion_list:
-                        if element.startswith(self.get().lower()):
-                                _hits.append(element)
-                # if we have a new hit list, keep this in mind
-                if _hits != self._hits:
-                        self._hit_index = 0
-                        self._hits=_hits
-                # only allow cycling if we are in a known hit list
-                if _hits == self._hits and self._hits:
-                        self._hit_index = (self._hit_index + delta) % len(self._hits)
-                # now finally perform the auto completion
-                if self._hits:
-                        self.delete(0,Tkinter.END)
-                        self.insert(0,self._hits[self._hit_index])
-                        self.select_range(self.position,Tkinter.END)
-                        
-        def handle_keyrelease(self, event):
-                """event handler for the keyrelease event on this widget"""
-                if event.keysym == "BackSpace":
-                        self.delete(self.index(Tkinter.INSERT), Tkinter.END) 
-                        self.position = self.index(Tkinter.END)
-                if event.keysym == "Left":
-                        if self.position < self.index(Tkinter.END): # delete the selection
-                                self.delete(self.position, Tkinter.END)
-                        else:
-                                self.position = self.position-1 # delete one character
-                                self.delete(self.position, Tkinter.END)
-                if event.keysym == "Right":
-                        self.position = self.index(Tkinter.END) # go to end (no selection)
-                if event.keysym == "Down":
-                        self.autocomplete(1) # cycle to next hit
-                if event.keysym == "Up":
-                        self.autocomplete(-1) # cycle to previous hit
-                # perform normal autocomplete if event is a single key or an umlaut
-                if len(event.keysym) == 1 or event.keysym in tkinter_umlauts:
-                        self.autocomplete()
-
-def test(test_list):
-        """Run a mini application to test the AutocompleteEntry Widget."""
-        root = Tkinter.Tk(className=' AutocompleteEntry demo')
-        entry = AutocompleteEntry(root)
-        entry.set_completion_list(test_list)
-        entry.pack()
-        entry.focus_set()
-        root.mainloop()
+def main():
+    try:
+        server = HTTPServer(('', 80), MyHandler)
+        print 'started httpserver...'
+        server.serve_forever()
+    except KeyboardInterrupt:
+        print '^C received, shutting down server'
+        server.socket.close()
 
 if __name__ == '__main__':
-        test_list = (u'test', u'type', u'true', u'tree', u'tölz')
-        print u"Type a 't' to test the AutocompleteEntry widget."
-        print u"Will use AutocompleteEntry.set_completion_list(%s)" % unicode(test_list)
-        print u"Try also the backspace key and the arrow keys."
-        test(test_list)
+    main()
+
